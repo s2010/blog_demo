7446f15192a12ca60f078e8a5db2acd64b0bdc89
s2010 <bashayer.hijji@gmail.com>
2016-04-08 04:56:49 +0300

    Added Post CRUD abilities

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+before_action :find_post, only: [:show , :edit, :update, :destroy]
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+  def create
+    @post =Post.new post_params
+    if @post.save
+      redirect_to @post, notice: "oh yeah your article is successfuly saved!"
+    else
+      render 'new', notice: "oh no, I was unable to save your post!"
+  end
+end
+def show
+end
+def edit
+end
+def update
+  if @post.update post_params
+    redirect_to @post, notice: "oh yeah your article is successfuly saved!"
+  else
+    render 'edit'
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+end
+  private
+  def post_params
+    params.require(:post).permit(:title,:content)
+  end
+  def find_post
+    @post = Post.find(params[:id])
+
+  end
+end
---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class PostsControllerTest < ActionController::TestCase
+  
+end
