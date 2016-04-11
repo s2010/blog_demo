9fc220174d8b2ecb8576d23192a77a37d415d17f
s2010 <bashayer.hijji@gmail.com>
2016-04-11 15:45:49 +0300

    Setup contact form

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ ruby '2.2.2'
   gem 'pygments.rb', '~> 0.6.3'
   gem 'friendly_id', '~> 5.1'
   gem 'will_paginate', '~> 3.1'
+  gem 'mail_form', '~> 1.5', '>= 1.5.1'
 
 group :development, :test do
   gem 'byebug'
---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < MailForm::Base
+ attribute :name,      :validate => true
+ attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+ attribute :message
+ attribute :nickname,  :captcha  => true
+
+   def headers
+     {
+      :subject => "Contact Form",
+      :to => "bashayer.hijji@gmail.com",
+      :from => %("#{name}" <#{email}>)
+     }
+ end
+end
