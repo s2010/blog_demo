010ae6d9e426b9bf1c67007d40f75f9c3827cb5b
s2010 <bashayer.hijji@gmail.com>
2016-04-11 08:31:15 +0300

    Add edit & destroy project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,7 +17,19 @@ class ProjectsController < ApplicationController
     end
   end
   def show
-
+  end
+  def edit
+  end
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: "Nice Bashayer! That project was successfuly updated!"
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
